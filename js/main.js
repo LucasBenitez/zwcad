ecf703a247c793cf3cd3d11fd360b4540a1fbad8
Tomas Gomez <talk.tomas@hotmail.com>
2020-07-06 16:51:30 -0300

    Creacion de mixins (SASS, PUG), modificacion de proyecto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("Main.js running")
+$(document).ready(function () {
+    $('.second-button').on('click', function () {
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+        $('.animated-icon3').toggleClass('open');
+        $(`#dropdownMenuMenu`).removeClass("active");
+    });
+});
+
+$(".dropdown-toggle").click(function(e){
+    let { target } = e;
+    console.log(target.id)
+    $(`#${target.id}`).toggleClass("active");
+});
